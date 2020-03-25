edeb02424f3686d0e89831e951019febcb01ac35
Administrator <Administrator@JUSIN-20180206V>
2020-03-25 19:26:58 +0900

    asd

---
+++ MFCTool/LineTool.h
@@ -0,0 +1,23 @@
+#pragma once
+
+
+// CLineTool 대화 상자입니다.
+
+class CLineTool : public CDialog
+{
+	DECLARE_DYNAMIC(CLineTool)
+
+public:
+	CLineTool(CWnd* pParent = NULL);   // 표준 생성자입니다.
+	virtual ~CLineTool();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_LINETOOL };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
